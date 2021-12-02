c85b9ff9d94c3329d1da2bddce1ca68d43376816
rcf7 <rodrigocampanaferrer@gmail.com>
2021-12-02 12:03:27 -0300

    comit initialize

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Piano';
+}
+
+
+aplicarSonido(numero : Number); void {
+  const audio = new Audio(),
+  audio.src = '../assets/sonidos/Sonido' + numero + '.mp3',
+  audio.load(),
+  audio.play(), 
+}   
+
+
+
+
