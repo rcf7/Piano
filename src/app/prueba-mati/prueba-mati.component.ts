147703db3d4d028c3824107d459ff873f4354020
Matias Carrion <matiasezequielcarrion@gmail.com>
2021-12-02 12:14:20 -0300

    agrego componente de prueba

---
+++ src/app/prueba-mati/prueba-mati.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prueba-mati',
+  templateUrl: './prueba-mati.component.html',
+  styleUrls: ['./prueba-mati.component.css']
+})
+export class PruebaMatiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
